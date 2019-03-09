bdeee111a9048d90c6c92c958152c36e4c3b19d2
Vishnu05 <vishnuthamiz96@gmail.com>
2019-03-09 13:11:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button> Hello react </button>
+    </div>
+  );
+}
+
+function Yo() {
+  return <p>This is my first function in react</p>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Yo />, rootElement);
